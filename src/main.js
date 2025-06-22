5a622682801b44c85ee51086a18cce4b71793a03
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-06-22 20:50:46 -0300

    comments

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 "use strict"
 
-// configuração do dayjs
 import "./libs/dayjs.js"
 
-// Importanção do CSS
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
 
-
-// Importação JS
 import "./modules/form/submit.js"
 import "./modules/page-load.js"
