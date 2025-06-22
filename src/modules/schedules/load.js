4b764695f39719761d11e0aaf772789c18fc7337
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-06-22 20:59:25 -0300

    Content loading event

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,15 +1,11 @@
 import { hoursLoad } from "../form/hours-load";
 
-// Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-// Busca na API os agendamentos para carregar do lado direito da tela.
-// Os horários disponíveis (horário futuro + não agendado) do lado esquerdo (form)
 export function schedulesDay() {
-  // Obtém a data do input
+
   const date = selectedDate.value
 
-  // Renderiza as horas disponíveis.
   hoursLoad({ date })
 
   
