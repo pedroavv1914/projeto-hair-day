55385e53dd2b91990987a1f8ea069f7ee8bea3d3
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-06-22 21:21:49 -0300

    Rendering the schedules

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }){
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(':')
@@ -9,7 +11,17 @@ export function hoursLoad({ date }){
 
     return {
       hour,
-      available: !isHourPast,
+      available: isHourPast,
     }
+  })
+
+  opening.forEach(({hour, available}) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+    hours.append(li)
   })
 }
